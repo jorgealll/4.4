10cb041260be2278ee7ba6600129365506c310ee
jorgealll <117639889+jorgealll@users.noreply.github.com>
2022-11-21 17:30:52 +0100

    cambio en main.java con mi nombre

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ public class Main {
      System.out.println("estudiante " + args[0]);
      System.out.println("matriculado en " + args[1] + " modulos");
      System.out.println("estudia " + args[2] + " horas al dia");
+        //jorge altet
     }
 }
