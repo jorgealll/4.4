430763b78bedb79e41a4605052c650756f2f6caa
jorgealll <117639889+jorgealll@users.noreply.github.com>
2022-11-21 17:26:49 +0100

    java modificado

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args){
-        
+     System.out.println("estudiante " + args[0]);
+     System.out.println("matriculado en " + args[1] + " modulos");
+     System.out.println("estudia " + args[2] + " horas al dia");
     }
 }
